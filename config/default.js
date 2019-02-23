17921e6d4bf5de506ac581e176622d1d034c1d8b
Whizark <contact@whizark.com>
2019-02-24 05:46:39 +0900

    add new `header-case`, `header-full-stop` rules
    conventional-changelog/commitlint@4c36cbd312254fb436dba25d0c6d44c64ae8ae1d

--- config/default.js
+++ config/default.js
@@ -3,135 +3,147 @@ const getConfig = require('commitlint-config-cz/lib/config').get;
 
 const config = {
     rules: {
-        'body-leading-blank'  : [
+        'body-leading-blank'    : [
             2,
             'always',
         ],
-        'body-max-length'     : [
+        'body-max-length'       : [
             1,
             'always',
             Infinity,
         ],
-        'body-max-line-length'     : [
+        'body-max-line-length'  : [
             1,
             'always',
             72,
         ],
-        'body-min-length'     : [
+        'body-min-length'       : [
             1,
             'always',
             0,
         ],
-        'footer-leading-blank': [
+        'footer-leading-blank'  : [
             2,
             'always',
         ],
-        'footer-max-length'   : [
+        'footer-max-length'     : [
             1,
             'always',
             Infinity,
         ],
-        'footer-max-line-length'   : [
+        'footer-max-line-length': [
             1,
             'always',
             72,
         ],
-        'footer-min-length'   : [
+        'footer-min-length'     : [
             1,
             'always',
             0,
         ],
-        'header-max-length'   : [
+        'header-case'           : [
+            1,
+            'always',
+            [
+                'lower-case',
+            ],
+        ],
+        'header-full-stop'      : [
+            2,
+            'never',
+            '.',
+        ],
+        'header-max-length'     : [
             1,
             'always',
             72,
         ],
-        'header-min-length'   : [
+        'header-min-length'     : [
             1,
             'always',
             0,
         ],
-        'references-empty'    : [
+        'references-empty'      : [
             0,
             'never',
         ],
-        'scope-enum'          : [
+        'scope-enum'            : [
             2,
             'always',
             [],
         ],
-        'scope-case'          : [
+        'scope-case'            : [
             2,
             'always',
             [
                 'lower-case',
             ],
         ],
-        'scope-empty'         : [
+        'scope-empty'           : [
             1,
             'never',
         ],
-        'scope-max-length'    : [
+        'scope-max-length'      : [
             1,
             'always',
             Infinity,
         ],
-        'scope-min-length'    : [
+        'scope-min-length'      : [
             1,
             'always',
             0,
         ],
-        'subject-case'        : [
+        'subject-case'          : [
             1,
             'always',
             [
                 'lower-case',
             ],
         ],
-        'subject-empty'       : [
+        'subject-empty'         : [
             2,
             'never',
         ],
-        'subject-full-stop'   : [
+        'subject-full-stop'     : [
             2,
             'never',
             '.'
         ],
-        'subject-max-length'  : [
+        'subject-max-length'    : [
             1,
             'always',
             Infinity,
         ],
-        'subject-min-length'  : [
+        'subject-min-length'    : [
             1,
             'always',
             0,
         ],
-        'type-enum'           : [
+        'type-enum'             : [
             2,
             'always',
             [],
         ],
-        'type-case'           : [
+        'type-case'             : [
             2,
             'always',
             'lower-case',
         ],
-        'type-empty'          : [
+        'type-empty'            : [
             2,
             'never',
         ],
-        'type-max-length'     : [
+        'type-max-length'       : [
             1,
             'always',
             Infinity,
         ],
-        'type-min-length'     : [
+        'type-min-length'       : [
             1,
             'always',
             0,
         ],
-        'signed-off-by'       : [
+        'signed-off-by'         : [
             0,
             'always',
         ],
