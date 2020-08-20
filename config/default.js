a70fd314f5a3ec84396846d3af0f9d42016ae115
Whizark <contact@whizark.com>
2020-08-20 17:36:11 +0900

    add an local plugin example as comment
    conventional-changelog/commitlint@7b29c48321b513e091849fbb2cc2bf0e6ebb94a6

--- config/default.js
+++ config/default.js
@@ -147,6 +147,15 @@ const config = {
             0,
             'always'
         ]
+        // ,
+        // 'hello-world-rule': ({subject}) => {
+        //     const HELLO_WORLD = 'Hello World';
+        //
+        //     return [
+        //         subject.includes(HELLO_WORLD),
+        //         `Your subject should contain ${HELLO_WORLD} message`
+        //     ];
+        // }
     }
 };
 
